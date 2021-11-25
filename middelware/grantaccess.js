831b3c4258f4680b1d895c59389d907723d6dfe8
TheisSchlanderNielsen <57490253+TheisSchlanderNielsen@users.noreply.github.com>
2021-11-25 15:01:42 +0100

    Update grantaccess.js
    updated grantaccess

--- middelware/grantaccess.js
+++ middelware/grantaccess.js
@@ -1,22 +1,37 @@
-const { roles } = require('../roles');
 
-const grantAccess = function(action, resource) {
-    return async (req, res, next) => {
-     try {
-    console.log(req.user.role);
+const AccessControl = require("accesscontrol");
+const ac = new AccessControl();
+
+const roles = (function () {
+  ac.grant("user")
+    .readOwn("account")
+    .updateOwn("account")
+    .createOwn("account")
+  ac.grant("admin")
+    .extend("user")
+    .readAny("account")
+    .updateAny("account")
+    .deleteAny("account")
+
+  return ac;
+})();
+const grantAccess = function (action, resource) {
+  return async (req, res, next) => {
+    try {
+      console.log(req.user.role);
       const permission = roles.can(req.user.role)[action](resource);
       if (!permission.granted) {
-       return res.status(401).json({
-        error: "You don't have enough permission to perform this action"
-       });
+        return res.status(401).json({
+          error: "You don't have enough permission to perform this action"
+        });
       }
       next()
-     } catch (error) {
-        console.log(error);
+    } catch (error) {
+      console.log(error);
       next(error)
-     }
     }
-   }
+  }
+}
 
 // Exports
 module.exports = grantAccess;
