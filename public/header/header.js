2225ec3a827c9ee8527528bc8ea1342837870c49
BenjaminPrehn <57618388+BenjaminPrehn@users.noreply.github.com>
2021-12-08 10:39:01 +0100

    Remove Employee button

--- public/header/header.js
+++ public/header/header.js
@@ -1,21 +1,34 @@
+(async function displayEmployeeButton() {
+    try {
+      $.ajax({
+        method: "GET",
+        url: "/users/me",
+        dataType: "json",
+      }).done(
+        function (user) {
 
+        if (user.role == "user")
+          document.getElementById("displayEmployees").style.display = "none";
+
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  })();
 
 function logout() {
-    try {
-        $.ajax({
-            method: "POST",
-            url: "/users/logout",
-            dataType: "json"
-        }, window.location.href = "/login"
-        ).done(
-
-        );
-    } catch (error) {
-        console.log(error);
-        alert(error);
-        
-    };
-};
-
-
+  try {
+    $.ajax(
+      {
+        method: "POST",
+        url: "/users/logout",
+        dataType: "json",
+      },
+      (window.location.href = "/login")
+    ).done();
+  } catch (error) {
+    console.log(error);
+    alert(error);
+  }
+}
 
